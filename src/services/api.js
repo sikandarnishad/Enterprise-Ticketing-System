d3c88ad3ddeb7cc8bfaed102b57ff166788735eb
Sikandar Nishad <109888740+sikandarnishad@users.noreply.github.com>
2026-08-28 22:30:16 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:7035/api",
+  baseURL: "https://enterprise-ticketing-api.onrender.com",
 });
 
 export default api;
